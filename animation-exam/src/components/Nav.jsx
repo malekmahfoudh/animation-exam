3ab9b766894e801a661ebc194444cd948ff90bda
malekmahfoudh <malek.mahfoudh7@me.com>
2023-10-24 10:45:09 +0200

    second commit

--- animation-exam/src/components/Nav.jsx
+++ animation-exam/src/components/Nav.jsx
@@ -1,40 +1,48 @@
 import React from "react";
-import { useState } from 'react';
+import { useState } from "react";
 import style from "./Nav.module.scss";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, spring } from "framer-motion";
 
 function Nav() {
-    const [isOpen, setIsOpen] = useState(false);
-  
-    const toggleMenu = () => {
-      setIsOpen(!isOpen);
-    };
-  
-    return (
-      <nav>
-        <button className={style.menuButton} onClick={toggleMenu}></button>
-        <AnimatePresence>
-          {isOpen && (
-            <motion.ul
-              initial={{ opacity: 0, x: -100 }}
-              animate={{ opacity: 1, x: 0 }}
-              exit={{ opacity: 0, x: -100 }}
-            >
-              <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-                <a href="/">Attractions</a>
-              </motion.li>
-              <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-                <a href="/about">Tickets</a>
-              </motion.li>
-              <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-                <a href="/contact">Contact</a>
-              </motion.li>
-            </motion.ul>
-          )}
-        </AnimatePresence>
-      </nav>
-    );
-  }
-  
-  export default Nav;
-  
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+
+  return (
+    <nav>
+      <button className={style.menuButton} onClick={toggleMenu}></button>
+      <AnimatePresence>
+        {isMenuOpen && (
+          <motion.ul
+            className={style.list}
+
+          >
+            <motion.li 
+                className={style.listItem}
+                whileHover={{ scale: 1.1 }} 
+                whileTap={{ scale: 0.9 }}>
+                <a href="/attractions">Attractions</a>
+            </motion.li>
+            <motion.li 
+                className={style.listItem}
+                whileHover={{ scale: 1.1 }} 
+                whileTap={{ scale: 0.9 }}>
+              <a href="/tickets">Tickets</a>
+            </motion.li>
+            <motion.li
+                className={style.listItem} 
+                whileHover={{ scale: 1.1 }} 
+                whileTap={{ scale: 0.9 }}>
+              <a href="/contact">Contact</a>
+            </motion.li>
+          </motion.ul>
+        )}
+      </AnimatePresence>
+    </nav>
+  );
+}
+
+export default Nav;
